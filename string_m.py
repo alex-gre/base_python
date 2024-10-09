a994ca74768ce9d7139ea697dcd7c55398c9b59a
alex-gre <40606584+alex-gre@users.noreply.github.com>
2024-10-09 10:10:29 +0300

    Add files via upload

---
+++ string_m.py
@@ -0,0 +1,18 @@
+s0 = " "
+s1 = "hello"
+s2 = "python"
+s3 = s1 + s0 + s2
+print('s3 = ',s3)
+
+# method list
+print('list(s3) = ',list(s3))
+# method set , set {множества} уникальные неповторяемые значения
+print('set(s3) = ',set(s3))
+l = list(s3) 
+s = set(s3)
+
+print('l = list(s3) => ',l)
+print('s = set(s3) => ',s)
+
+
+
